15930ae9feed8129038d6ede9aba91f4e73bd100
tintoll <belselios@gmail.com>
2019-07-15 08:48:29 +0900

    enzyme 이용한 테스트

---
+++ react-enzyme-test/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <div>
+      <Profile username="tintoll" name="화니" />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-enzyme-test/src/Profile.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { mount } from "enzyme";
+import Profile from './Profile';
+
+describe('<Profile />', () => {
+
+  it('matches snapshot', () => {
+    // Enzyme 을 통하여 리액트 컴포넌트를 렌더링 해줍니다.
+    const wrapper = mount(<Profile username="tintoll" name="화니" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders username and name', () => {
+    const wrapper = mount(<Profile username="tintoll" name="화니" />);
+    
+    expect(wrapper.props().username).toBe('tintoll');
+    expect(wrapper.props().name).toBe('화니');
+
+    // find 함수를 사용하면 특정 DOM을 선택할 수 있다. querySelector와 똑같은 방식으로 사용할 수 있다.
+    const boldElement = wrapper.find('b');
+    expect(boldElement.contains('tintoll')).toBe(true);
+
+    const spanElement = wrapper.find('span');
+    expect(spanElement.text()).toBe('(화니)');
+  })
+});
